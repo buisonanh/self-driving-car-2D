45560d7035496a6b6a867cbd9d71d4487e992f98
Son Anh <sonanhbui2407@gmail.com>
2023-03-05 01:31:18 +0700

    ok

--- car.js
+++ car.js
@@ -17,9 +17,11 @@ class Car{
     }
 
     update(roadBorders){
-        this.#move();
-        this.polygon=this.#createPolygon();
-        this.damaged=this.#assessDamage(roadBorders);
+        if(!this.damaged){
+            this.#move();
+            this.polygon=this.#createPolygon();
+            this.damaged=this.#assessDamage(roadBorders);
+        }
         this.sensor.update(roadBorders);
     }
 
@@ -95,6 +97,11 @@ class Car{
     }
 
     draw(ctx){
+        if(this.damaged){
+            ctx.fillStyle="gray";
+        }else{
+            ctx.fillStyle="black";
+        }
         ctx.beginPath();
         ctx.moveTo(this.polygon[0].x,this.polygon[0].y);
         for(let i=1;i<this.polygon.length;i++){
